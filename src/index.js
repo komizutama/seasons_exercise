632358a8fecc2b184249eb42b64bb50276c0a38c
komizutama <komizutama@gmail.com>
2021-03-25 17:43:28 -0400

    Added set state. Shows latitude.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ class App extends React.Component {
     constructor (props) {
         super(props);
 
+        //The initial declaration is the only time we directly declare state
         this.state={lat: null };
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({lat: position.coords.latitude});
+            },//updates the state
+            (err) => console.log(err)
+        );
     };
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-            (err) => console.log(err)
-        );
-
-        return <div> latitude: </div>;
+        return <div>latitude: {this.state.lat}</div>;
     }
 }
 
