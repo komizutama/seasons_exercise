36cdaf4e36bb193bc69409b2af96ebc6e8d9d6cf
komizutama <komizutama@gmail.com>
2021-03-27 11:47:21 -0400

    added ternary phrases to determine if it's summer side of the equinox or not.

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,7 +1,18 @@
- import React from 'react';
+import React from 'react';
 
- const SeasonDisplay =() => {
-     return <div>SeasonDisplay</div>;
+const getSeason = (lat, month) => {
+    if ( month > 2 && month < 9) {
+        return lat > 0 ? 'summer' : 'winter';
+    } else {
+        return lat < 0 ? 'winter' : 'summer';
+    }
+}
+
+const SeasonDisplay =(props) => {
+    const season = getSeason(props.lat, new Date().getMonth());
+    const text = season === 'winter' ? 'Burr, it is chilly!' : 'Lets hit the beach!';
+    
+    return <h1>{ text }</h1>;
  };
 
 export default SeasonDisplay;
