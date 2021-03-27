6c497b6166b0e21fb80b0673474209d0bad4e026
komizutama <komizutama@gmail.com>
2021-03-27 13:50:49 -0400

    css mods and adding a loader a swell as abstracting out a div into renderContent to let us seperate visual components.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 
 class App extends React.Component {
@@ -13,15 +14,24 @@ class App extends React.Component {
         );
     }
 
-    render() {
+    renderContent() {
         if(this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
+            return <div>Error: {this.state.errorMessage}</div>;
         }
         
         if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
+            return <SeasonDisplay lat={this.state.lat} />;
         }
-        return <div>Loading!</div>
+        return <Spinner  message="Please Accept Location Request!" />;
+    }
+// moving logic to renderContent and adding that div into render lets that compoent be abstracted and keep the ability to have a red border no matter what... even though we didn do the css for it.
+
+    render() {
+        return (
+            <div className='border red'>
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
